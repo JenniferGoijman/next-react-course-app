b66c753aaabbb85bbcb4070f9ec85a3dad792491
Jennifer Goijman <jennifer@journi.uk>
2024-04-23 17:41:56 +0200

    feat: displaying meal details

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
-export default function SlugPage ({ params }) {
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.slug);
+
+  const { instructions, image, title, summary, creator_email, creator } = meal;
+
+  const formattedInstructions = instructions.replace(/\n/g, '<br />');
+
   return (
-    <main>
-      <h1>Slug Page - {params.slug}</h1>
-    </main>
-  )
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={classes.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: formattedInstructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
